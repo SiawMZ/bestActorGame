704c374e6da46797d99dda96b5fc9d13f501c9dc
SiawMZ <mingzhe1998@gmail.com>
2024-04-12 17:46:41 +0800

    added more data and css

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -21,27 +21,38 @@ export default function Question() {
 
   return (
     <>
-      <div className="m-44 grid justify-items-center">
-        <div>
-          <h1 className="text-5xl font-bold text-center">
-            对白： {currentItem.dialog}
-          </h1>
-          <p className="mt-5 text-3xl font-bold text-center">
-            请问：{currentItem.Question}
-          </p>
-          {linesOrder.map((index) => (
-            <p key={index}>
-              {index === 0 && (
-                <span style={{ color: "green" }}>{currentItem.true}</span>
-              )}
-              {index === 1 && currentItem.false}
-              {index === 2 && currentItem["false 2"]}
+      <div className="m-28 grid justify-items-center ">
+        <div className="shadow-xl rounded-xl">
+          <div className="bg-black rounded-t-xl px-10 py-11">
+            <h1 className="text-5xl font-bold text-center text-white">
+              题目： {currentItem.dialog}
+            </h1>
+            <p className="mt-5 text-3xl font-bold text-center text-white">
+              请问：{currentItem.Question}
             </p>
-          ))}
-        </div>
-
-        <div className="m-9">
-          <button onClick={nextQuestion}>Next Question</button>
+          </div>
+          {/* answer */}
+          <div className=" text-center text-2xl font-normal tracking-wider leading-relaxed py-11 pb-24 rounded-b-xl bg-white">
+            {linesOrder.map((index) => (
+              <p key={index}>
+                {index === 0 && (
+                  <span className="text-green-600 font-extrabold">
+                    {currentItem.true}
+                  </span>
+                )}
+                {index === 1 && currentItem.false}
+                {index === 2 && currentItem["false 2"]}
+              </p>
+            ))}
+          </div>
+          <div className="pb-10 grid justify-items-center ">
+            <button
+              className="px-8 z-30 py-4 border-2 border-black text-black relative font-semibold font-sans after:-z-20 after:absolute after:h-full after:w-1 after:bg-black hover:text-white after:-left-1 overflow-hidden after:translate-y-full after:hover:scale-[200] after:hover:transition-all after:hover:duration-500 after:transition-all after:duration-500 transition-all duration-500 text-2xl"
+              onClick={nextQuestion}
+            >
+              Next Question
+            </button>
+          </div>
         </div>
       </div>
     </>
